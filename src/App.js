c22726c3850d24e211d78710c7820319a8f0a882
Karikaran vettivel <karikaran@paraqum.com>
2022-06-24 02:05:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { WonderPush } from 'react-wonderpush';
   import './App.css';
+  import SwitchButton from './SwitchButton'
 
 function App() {
   return (
    // Wrap your app with our WonderPush component
    <WonderPush options = {{webKey: "eb6fafdce2dc916c828bd68ae984fa7b5d5e0826ac8ba33c420a5bc68bd08c89"}}>
+  
    <div className="App">
      <h1>here my componet</h1>
    </div>
+   <SwitchButton/>
  </WonderPush>
   );
 }         
---
+++ src/SwitchButton.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+function SwitchButton() {
+    return (
+        <div className="switch-container">
+            <div id="wonderpush-subscription-switch" data-on="YES" data-off="NO"></div>
+          </div>
+    );
+  }   
+
+ 
+
+export default SwitchButton;    
