5387acbeaefabec51d4d636de3608805437fbf92
Karikaran vettivel <karikaran@paraqum.com>
2022-06-24 19:29:50 +0530

    first commit

--- src/SwitchButton.js
+++ src/SwitchButton.js
@@ -3,7 +3,9 @@ import React,{Component} from 'react';
 function SwitchButton() {
     return (
         <div className="switch-container">
-            <div id="wonderpush-subscription-switch" data-on="YES" data-off="NO"></div>
+            <div id="wonderpush-subscription-switch"
+        data-sentence="Subscribe to notifications: "></div>
+
           </div>
     );
   }   
