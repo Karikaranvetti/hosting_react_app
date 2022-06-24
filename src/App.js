1653226591781560ec517277fa36c058be5958a7
Karikaran vettivel <karikaran@paraqum.com>
2022-06-24 20:04:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { WonderPush } from 'react-wonderpush';
   import './App.css';
   import SwitchButton from './SwitchButton'
+  import withWonderPush from './withWonderPush';
 
 function App() {
+
+  withWonderPush()
   return (
    // Wrap your app with our WonderPush component
    <WonderPush options = {{webKey: "eb6fafdce2dc916c828bd68ae984fa7b5d5e0826ac8ba33c420a5bc68bd08c89"}}>
