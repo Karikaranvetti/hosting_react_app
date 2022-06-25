6c52fea8cf8e4b85e3ec87c98a09e6807b25d4b7
Karikaran vettivel <karikaran@paraqum.com>
2022-06-25 11:46:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import { WonderPush } from 'react-wonderpush';
-  import './App.css';
-  import SwitchButton from './SwitchButton'
-  import EventToTrack from './withWonderPush';
+import { useEffect } from 'react';
+import './App.css';
+import SwitchButton from './SwitchButton'
+import EventToTrack from './withWonderPush';
+import { appendScript } from './appendScript';
 
- 
+
+
+// appendScript("../public/wonderpush-sdk-files/wonderpush-worker-loader.min.js");
 function App() {
-
-  // withWonderPush(SwitchButton);
-  return (
-   // Wrap your app with our WonderPush component
-   <WonderPush options = {{webKey: "eb6fafdce2dc916c828bd68ae984fa7b5d5e0826ac8ba33c420a5bc68bd08c89" }}>
+  useEffect(() => {
+    const script = document.createElement('script');
   
-   <div className="App">
-     <h1>here my componet</h1>
-     <a href="#" onclick="WonderPush.subscribeToNotifications(event); return false;">Click me to subscribe</a>
+    script.src = "../public/wonderpush-sdk-files/wonderpush-worker-loader.min.js";
+    script.async = true;
+  
+    document.body.appendChild(script);
+  
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, []);
+  return (
+    // Wrap your app with our WonderPush component
+    <WonderPush options={{ webKey: "eb6fafdce2dc916c828bd68ae984fa7b5d5e0826ac8ba33c420a5bc68bd08c89" }}>
 
-   </div>
-   <SwitchButton/>
-   {/* <EventToTrack/> */}
- </WonderPush>
+      <div className="App">
+        <h1>here my componet</h1>
+        <a href="#" onclick="WonderPush.subscribeToNotifications(event); return false;">Click me to subscribe</a>
+
+      </div>
+      <SwitchButton />
+      {/* <EventToTrack/> */}
+    </WonderPush>
   );
-}         
+}
 
 export default App;
