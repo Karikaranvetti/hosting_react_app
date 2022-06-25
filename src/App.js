bd7c5f4c062b1f77b58c09604b6183a6d47bdeae
Karikaran vettivel <karikaran@paraqum.com>
2022-06-25 11:48:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   useEffect(() => {
     const script = document.createElement('script');
   
-    script.src = "../public/wonderpush-sdk-files/wonderpush-worker-loader.min.js";
+    script.src = "../public/wonderpush-worker-loader.min.js";
     script.async = true;
   
     document.body.appendChild(script);
@@ -26,7 +26,7 @@ function App() {
     <WonderPush options={{ webKey: "eb6fafdce2dc916c828bd68ae984fa7b5d5e0826ac8ba33c420a5bc68bd08c89" }}>
 
       <div className="App">
-        <h1>here my componet</h1>
+        <h1>here my componet2</h1>
         <a href="#" onclick="WonderPush.subscribeToNotifications(event); return false;">Click me to subscribe</a>
 
       </div>
